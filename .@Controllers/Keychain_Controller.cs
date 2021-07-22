3677e8af76ecbc285e958f8ca33660262138e83d
kek-sec <georgepetrakis99@gmail.com>
2021-07-22 19:31:13 +0300

    Keychain Controller work

--- .@Controllers/Keychain_Controller.cs
+++ .@Controllers/Keychain_Controller.cs
@@ -71,5 +71,31 @@ namespace PGPKeys____Pretty_Good_Privacy_utility
 
             }
         }
+
+        public void Listbox_Item_Click(KeyChainObject selected_key,Button keychain_emcrypt_btn,Button keychain_decrypt_btn,Button keychain_verify_btn,Button keychain_sign_btn,ListBox keychain_listbox)
+        {
+            var indx = keychain_listbox.SelectedIndex;
+            if (indx == -1) { return; }
+            //set current key
+            selected_key.email = keychain.keyChainList[indx].email;
+            selected_key.private_key = keychain.keyChainList[indx].private_key;
+            selected_key.public_key = keychain.keyChainList[indx].public_key;
+
+            if (selected_key.private_key == null)
+            {
+                keychain_emcrypt_btn.Enabled = true;
+                keychain_decrypt_btn.Enabled = false;
+                keychain_verify_btn.Enabled = true;
+                keychain_sign_btn.Enabled = false;
+            }
+            else
+            {
+                keychain_emcrypt_btn.Enabled = true;
+                keychain_decrypt_btn.Enabled = true;
+                keychain_verify_btn.Enabled = true;
+                keychain_sign_btn.Enabled = true;
+
+            }
+        }
     }
 }
--- .@Forms/MainForm.cs
+++ .@Forms/MainForm.cs
@@ -63,28 +63,7 @@ namespace PGPKeys____Pretty_Good_Privacy_utility
 
         private void keychain_listbox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var indx = keychain_listbox.SelectedIndex;
-            if (indx == -1) { return; }
-            //set current key
-            selected_key.email = keychain.keyChainList[indx].email;
-            selected_key.private_key = keychain.keyChainList[indx].private_key;
-            selected_key.public_key = keychain.keyChainList[indx].public_key;
-
-            if (selected_key.private_key == null)
-            {
-                keychain_emcrypt_btn.Enabled = true;
-                keychain_decrypt_btn.Enabled = false;
-                keychain_verify_btn.Enabled = true;
-                keychain_sign_btn.Enabled = false;
-            }
-            else
-            {
-                keychain_emcrypt_btn.Enabled = true;
-                keychain_decrypt_btn.Enabled = true;
-                keychain_verify_btn.Enabled = true;
-                keychain_sign_btn.Enabled = true;
-
-            }
+            kc.Listbox_Item_Click(selected_key, keychain_emcrypt_btn, keychain_decrypt_btn, keychain_verify_btn, keychain_sign_btn, keychain_listbox);
 
         }
 
