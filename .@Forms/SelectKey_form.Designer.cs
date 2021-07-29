a2c9ef5a72bc26dec72bc61036fa641c8c2c9094
kek-sec <georgepetrakis99@gmail.com>
2021-07-29 20:39:09 +0300

    work on selectKey_form

--- .@Forms/SelectKey_form.Designer.cs
+++ .@Forms/SelectKey_form.Designer.cs
@@ -29,25 +29,38 @@ namespace PGPKeys____Pretty_Good_Privacy_utility
         /// </summary>
         private void InitializeComponent()
         {
+            this.select_key_listbox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // select_key_listbox
+            // 
+            this.select_key_listbox.FormattingEnabled = true;
+            this.select_key_listbox.ItemHeight = 25;
+            this.select_key_listbox.Location = new System.Drawing.Point(3, 2);
+            this.select_key_listbox.Name = "select_key_listbox";
+            this.select_key_listbox.Size = new System.Drawing.Size(521, 454);
+            this.select_key_listbox.TabIndex = 0;
             // 
             // SelectKey_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(693, 481);
-            this.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ClientSize = new System.Drawing.Size(527, 459);
+            this.Controls.Add(this.select_key_listbox);
+            this.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "SelectKey_form";
-            this.Text = "Settings";
+            this.Text = "Select key";
+            this.Load += new System.EventHandler(this.SelectKey_form_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.ListBox select_key_listbox;
     }
 }
 
--- .@Forms/SelectKey_form.cs
+++ .@Forms/SelectKey_form.cs
@@ -7,16 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
 
 namespace PGPKeys____Pretty_Good_Privacy_utility
 {
     public partial class SelectKey_form : Form
     {
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
         SettingsService settings = new SettingsService();
+        KeyChainService kcs = new KeyChainService();
 
         public SelectKey_form()
         {
             InitializeComponent();            
         }
+
+        private void SelectKey_form_Load(object sender, EventArgs e)
+        {
+            kcs.LoadKeyChain(settings.getSetting("keys_folder_path"));
+            List<KeyChainObject> keychain = kcs.keyChainList;
+
+            foreach(KeyChainObject key_set in keychain)
+            {
+                select_key_listbox.Items.Add(key_set.email);
+            }
+
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
     }
+
 }
